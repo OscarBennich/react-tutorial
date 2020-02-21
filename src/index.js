a8d4d0cd7ea0961a247e5f2507b3bf5498950b7e
Oscar Bennich-Björkman <oscar.bennich@gmail.com>
2020-02-21 23:56:33 +0100

    Currently selected step now bolded in list

--- src/index.js
+++ src/index.js
@@ -85,8 +85,9 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ? 
-      'Go to move # ' + move : 
+      const desc = move && this.state.stepNumber === move ? 
+      <b>'Go to move # ' + {move}</b>: move ?
+      'Go to move # ' + move :
       'Go to game start';
       return (
         <li key={move}>
